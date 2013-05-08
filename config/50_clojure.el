966dcde11a8412c4d998f49cfd86911b605eb8e1
kou <kou.morimoto@gmail.com>
2013-05-08 18:41:34 +0900

    temporary setting

--- config/50_clojure.el
+++ config/50_clojure.el
@@ -10,3 +10,21 @@
 (add-hook 'clojure-mode-hook
 	  '(lambda ()
 	     (nrepl-eldoc-enable-in-current-buffer)))
+
+(el-get 'sync '(ac-nrepl))
+
+(require 'nrepl)
+(require 'ac-nrepl)
+
+(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+(add-hook 'clojure-nrepl-mode-hook 'ac-nrepl-setup)
+
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'nrepl-mode))
+
+;; setting for auto-complete in repl
+(defun set-auto-complete-as-completion-at-point-function ()
+  (setq completion-at-point-functions '(auto-complete)))
+(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'nrepl-interaction-mode 'set-auto-complete-as-completion-at-point-function)
