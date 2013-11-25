00d9f0b742c8245f1e89def22e37ff9bbd6a172b
kou <kou.morimoto@gmail.com>
2013-11-26 06:57:49 +0900

    rename 10_auto-async-byte-compile.el to 10_byte-compile.el

--- config/10_auto-async-byte-compile.el
+++
@@ -1,6 +0,0 @@
-(el-get 'sync '(auto-async-byte-compile))
-
-(require 'auto-async-byte-compile)
-
-(setq auto-async-byte-compile-exclude-files-regexp "/junk/")
-(add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
---
+++ config/10_byte-compile.el
@@ -0,0 +1,21 @@
+(el-get 'sync '(auto-async-byte-compile))
+
+;; auto-async-byte-compile
+(require 'auto-async-byte-compile)
+(setq auto-async-byte-compile-exclude-files-regexp "/junk/")
+(add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
+
+;; auto byte compile when *.el newer than *.elc
+(when (and user-init-file
+	   (equal (file-name-extension user-init-file) "elc"))
+  (let* ((source (file-name-sans-extension user-init-file))
+	 (alt (concat source ".el")))
+    (setq source (cond ((file-exists-p alt) alt)
+		       ((file-exists-p source) source)
+		       (t nil)))
+    (when source
+      (when (file-newer-than-file-p source user-init-file)
+	(byte-compile-file source)
+	(load-file source)
+	(eval-buffer nil nil)
+        (delete-other-windows)))))
