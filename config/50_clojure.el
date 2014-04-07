0383ae80c82e33ceab4dfe6b67d8d587b0d08381
森本浩介 <kosuke@iMac.local>
2014-04-08 00:21:17 +0900

    bug fix

--- config/50_clojure.el
+++ config/50_clojure.el
@@ -1,30 +1,14 @@
 (el-get 'sync '(clojure-mode))
 (el-get 'sync '(paredit))
-(el-get 'sync '(paredit-extension))
+(el-get 'sync '(cider))
+(el-get 'sync '(rainbow-delimiters))
 
 (require 'clojure-mode)
 (require 'paredit)
+(require 'cider)
+(require 'rainbow-delimiters)
 
-(defun turn-on-paredit () (paredit-mode 1))
-(add-hook 'clojure-mode-hook 'turn-on-paredit)
-(add-hook 'clojure-mode-hook
-	  '(lambda ()
-	     (nrepl-eldoc-enable-in-current-buffer)))
-
-(el-get 'sync '(ac-nrepl))
-
-(require 'nrepl)
-(require 'ac-nrepl)
-
-(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
-(add-hook 'clojure-nrepl-mode-hook 'ac-nrepl-setup)
-
-(eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'nrepl-mode))
-
-;; setting for auto-complete in repl
-(defun set-auto-complete-as-completion-at-point-function ()
-  (setq completion-at-point-functions '(auto-complete)))
-(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
-(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
-(add-hook 'nrepl-interaction-mode 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'clojure-mode-hook 'parendit-mode)
+(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
+(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
